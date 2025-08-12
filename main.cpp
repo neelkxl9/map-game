f5a7a2cbc248e2c55fb312f24c4ace997fcc7889
neelkxl9 <kadamneel070406@gmail.com>
2025-08-12 20:29:03 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+// Convert string to lowercase
+string toLowerCase(string str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return str;
+}
+
+int main() {
+    vector<string> countries = {
+        "India", "USA", "Brazil", "Australia", "Canada",
+        "Japan", "Germany", "France", "Russia", "South Africa"
+    };
+
+    srand(time(0));
+    string secret = countries[rand() % countries.size()];
+
+    cout << "=== MAP GAME ===\n";
+    cout << "Guess the country!\n";
+
+    string guess;
+    while (true) {
+        cout << "Your guess: ";
+        getline(cin, guess);
+
+        if (toLowerCase(guess) == toLowerCase(secret)) {
+            cout << "Correct! It was " << secret << ".\n";
+            break;
+        } else {
+            cout << "Wrong! Try again.\n";
+        }
+    }
+}
